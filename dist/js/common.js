0e17257446ec04549032952c7b0fa6b156437f4c
vicky <994312463@qq.com>
2017-08-30 23:06:44 +0800

    'update'

--- dist/js/common.js
+++ dist/js/common.js
@@ -4,10 +4,11 @@ var vue = new Vue({
         return {
             page: 1,
             time: 0.000,
+            curTime: 0.000,
             isHide: false,
             isBgHide: true,
-            right: false,
-            wrong: false,
+            isRight: false,
+            isWrong: false,
             question: [
                 {
                     title: "谁在小红录歌时溜进了广播室？",
@@ -21,7 +22,7 @@ var vue = new Vue({
                     title: "辛夷用哪个字来形容秋水？",
                     sec1Img: "https://gw.alicdn.com/mt/TB1tWPRSpXXXXaeXpXXXXXXXXXX-320-540.jpg",
                     sec1Tag: "贱",
-                    scet2Img: "https://gw.alicdn.com/mt/TB1DbLBSpXXXXcvXFXXXXXXXXXX-320-540.jpg",
+                    sec2Img: "https://gw.alicdn.com/mt/TB1DbLBSpXXXXcvXFXXXXXXXXXX-320-540.jpg",
                     sec2Tag: "污",
                     answer: "贱"
                 },
@@ -29,7 +30,7 @@ var vue = new Vue({
                     title: "秋水痴迷于哪项活动？",
                     sec1Img: "https://gw.alicdn.com/mt/TB1q8nESpXXXXcGXFXXXXXXXXXX-320-540.jpg",
                     sec1Tag: "看书",
-                    scet2Img: "https://gw.alicdn.com/mt/TB1z_W8SpXXXXcKaFXXXXXXXXXX-320-540.jpg",
+                    sec2Img: "https://gw.alicdn.com/mt/TB1z_W8SpXXXXcKaFXXXXXXXXXX-320-540.jpg",
                     sec2Tag: "削土豆",
                     answer: "看书"
                 },
@@ -37,7 +38,7 @@ var vue = new Vue({
                     title: "小红认为全校最美的女生是？",
                     sec1Img: "https://gw.alicdn.com/mt/TB1YK2hSpXXXXXoapXXXXXXXXXX-320-540.jpg",
                     sec1Tag: "赵英来",
-                    scet2Img: "https://gw.alicdn.com/mt/TB1uXvmSpXXXXXXapXXXXXXXXXX-320-540.jpg",
+                    sec2Img: "https://gw.alicdn.com/mt/TB1uXvmSpXXXXXXapXXXXXXXXXX-320-540.jpg",
                     sec2Tag: "她自己",
                     answer: "她自己"
                 },
@@ -45,7 +46,7 @@ var vue = new Vue({
                     title: "小红为何一直整蛊秋水？",
                     sec1Img: "https://gw.alicdn.com/mt/TB1uyW8SpXXXXclaFXXXXXXXXXX-320-540.jpg",
                     sec1Tag: "爱开玩笑",
-                    scet2Img: "https://gw.alicdn.com/mt/TB1v1TESpXXXXaRXFXXXXXXXXXX-320-540.jpg",
+                    sec2Img: "https://gw.alicdn.com/mt/TB1v1TESpXXXXaRXFXXXXXXXXXX-320-540.jpg",
                     sec2Tag: "和谈失败",
                     answer: "和谈失败"
                 }
@@ -59,6 +60,7 @@ var vue = new Vue({
     },
     methods: { 
         start() {  
+            this.time = 0.000
             this.isHide = true
             console.log(this.page)
             let that = this
@@ -67,28 +69,68 @@ var vue = new Vue({
                 that.page = 2
             }, 2500)
             var allTime = setTimeout(() => {
-              that.timedCount()
+               if(this.page == 6 ) {
+                    clearTimeout(allTime)
+                }else {
+                    that.timedCount()
+                }
             }, 0.0001)
-            
+           
         },
         timedCount() {
             let that = this
-            setInterval(() => {
-              that.time =that.time + 0.001
+            var t = setInterval(() => {
+                if(this.page == 6) {
+                     clearInterval(t)
+                 } else {
+                    that.time =that.time + 0.001
+                 }
             }, 0.1)
+           
         },
-        trueAn() {
-            // this.page = this.page + 1
-            console.log('正确')
+        trueAn() {       
+            //   console.log(this.page)       
+            // if(this.page == 5) {
+            //     setTimeout(() => {
+            //         this.page = 6
+            //       }, 520)
+            //     this.curTime = this.time
+            //     console.log(this.curTime)
+            //     // clearTimeout(allTime)
+            //     // clearInterval(t)
+            // } else {
+                this.isRight = true
+                setTimeout(() => {
+                    this.isRight = false
+                }, 500)
+                setTimeout(() => { 
+                    this.page = this.page + 1        
+                }, 520)
+                // this.curTime = this.time
+                // if(this.page = 6) {
+                    // this.curTime = this.time
+                    // clearTimeout(allTime)
+                    // clearInterval(t)
+                // }
         },
         falseAn() {
-            this.page = 6
-            console.log(this.page)
-            console.log('错误')
+            this.isWrong = true
+            Promise.resolve(
+                setTimeout(() => {
+                    this.isWrong = false
+                }, 500)
+            )
+            Promise.resolve(
+                  setTimeout(() => {
+                    this.page = 7
+                  }, 520) 
+            )
+            this.time = 0.000
         },
         again() {
             this.page = 1
         }
+
     }
 })
 
